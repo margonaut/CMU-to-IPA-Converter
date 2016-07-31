8ecfe6dffe243cfaadc592de842be4aba704fd10
margonaut <margaret.morgane@gmail.com>
2016-07-31 11:58:02 -0400

    Code cleanup:

--- script.rb
+++ script.rb
@@ -2,13 +2,11 @@ require 'csv'
 require 'pry'
 require_relative 'cmu_ipa_mapping'
 
-
 CSV.open("assets/output.csv", "wb") do |csv|
   CSV.foreach('assets/test_cmu.csv') do |row|
     word = row.shift
     result = row.map { |cmu| CMU_IPA_MAPPING[cmu.to_sym] }
     result.unshift(word)
     csv << result
-
   end
 end
