e3a7666712db542156e0cac8b55d13e081188f01
margonaut <margaret.morgane@gmail.com>
2016-07-31 11:53:24 -0400

    CMU to IPA transcriptor working

--- script.rb
+++ script.rb
@@ -5,8 +5,10 @@ require_relative 'cmu_ipa_mapping'
 
 CSV.open("assets/output.csv", "wb") do |csv|
   CSV.foreach('assets/test_cmu.csv') do |row|
-    # binding.pry
-    # csv << COLUMNS.map { |col| row[col] }
-    csv << row
+    word = row.shift
+    result = row.map { |cmu| CMU_IPA_MAPPING[cmu.to_sym] }
+    result.unshift(word)
+    csv << result
+
   end
 end
