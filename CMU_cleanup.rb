08b8b30f6c443864a423b211323a0b7ae4174f9d
margonaut <margaret.morgane@gmail.com>
2016-07-31 12:08:42 -0400

    Added script to clean up the raw CMU dictionary into a csv

---
+++ CMU_cleanup.rb
@@ -0,0 +1,12 @@
+require 'csv'
+require 'pry'
+require_relative 'cmu_ipa_mapping'
+
+
+CSV.open("assets/CMU_dictionary.csv", "wb") do |csv|
+  File.open('assets/CMU_dictionary.txt', 'r') do |f|
+    f.each_line do |line|
+      csv << line.split(" ")
+    end
+  end
+end
