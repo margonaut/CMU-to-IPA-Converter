c6e89120fe4f3fd3f98ed8f8040611dbc21f947b
margonaut <margaret.morgane@gmail.com>
2016-07-31 11:38:21 -0400

    CMU to IPA mapping variable set up

--- script.rb
+++ script.rb
@@ -1,8 +1,12 @@
 require 'csv'
 require 'pry'
+require_relative 'cmu_ipa_mapping'
+
 
 CSV.open("assets/output.csv", "wb") do |csv|
   CSV.foreach('assets/test_cmu.csv') do |row|
+    # binding.pry
+    # csv << COLUMNS.map { |col| row[col] }
     csv << row
   end
 end
