04b1024c99752afbb53f55bb554d1ff2bab3723a
margonaut <margaret.morgane@gmail.com>
2016-07-31 12:19:31 -0400

    Added a regex matcher that excludes alternate pronunciation lines

--- CMU_cleanup.rb
+++ CMU_cleanup.rb
@@ -6,7 +6,9 @@ require_relative 'cmu_ipa_mapping'
 CSV.open("assets/CMU_dictionary.csv", "wb") do |csv|
   File.open('assets/CMU_dictionary.txt', 'r') do |f|
     f.each_line do |line|
-      csv << line.split(" ")
+      unless /(\(\d\))/.match(line)
+        csv << line.split(" ")
+      end
     end
   end
 end
